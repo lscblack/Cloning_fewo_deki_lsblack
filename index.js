6f8b71c8eadecb98f91996e64db2aba30d712094
lscblack <l.christian@alustudent.com>
2024-11-08 13:43:28 +0200

    done with JS One and second part

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ window.onscroll = (event) => {
         scrollDiv.classList.add("top-0 sticky");
     }
 };
+// Toggle Menu
+const toggleMenu = () => {
+    const menu = document.getElementById("menu");
+    if (menu.classList.contains("max-lg:hidden")) {
+        menu.classList.remove("max-lg:hidden");
+        menu.classList.add("max-lg:block");
+    } else {
+        menu.classList.add("max-lg:hidden");
+        menu.classList.remove("max-lg:block bg-blue fixed top-10 ");
+    }
+};
+
+//loop for first images grid
+const GridImageOne = [
+    {
+        "image":"",
+        "name":"",
+    }
+]
